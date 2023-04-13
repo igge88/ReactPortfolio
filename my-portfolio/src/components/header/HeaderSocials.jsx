2e6b5d1001168bb62d2bb67aedebcd1f06bf2c4d
igge <igge88@hotmail.com>
2023-04-13 20:59:18 +0200

    push4

--- my-portfolio/src/components/header/HeaderSocials.jsx
+++ my-portfolio/src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {FiTwitter} from 'react-icons/fi'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/ignacio-tapia-troncoso-01b07710b/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/igge88" target="_blank"><FaGithub/></a>
-        <a href="https://www.twitter.com" target="_blank"><FiTwitter/></a>
+        <a href="https://www.linkedin.com/in/ignacio-tapia-troncoso-01b07710b/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/igge88" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><FiTwitter/></a>
     </div>
   )
 }
