35b04bfce08814520817a2400c265e11663b9784
igge <igge88@hotmail.com>
2023-04-16 22:17:53 +0200

    push6

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -3,8 +3,21 @@ import './contact.css'
 import {HiOutlineMail} from 'react-icons/hi'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_vf8f1op', 'template_y91e6f7', form.current, 'U6MVNBwV6eImtSF7H')
+
+        e.target.reset()
+      };
   return (
     <section id='contact'>
         <h5>Get In Touch</h5>
@@ -16,23 +29,23 @@ const Contact = () => {
                     <HiOutlineMail className='contact_option-icon'/>
                     <h4>Email</h4>
                     <h5>igge88@hotmail.com</h5>
-                    <a href="mailto:igge88@hotmail.com" target='_blank'>Email me</a>
+                    <a href="mailto:igge88@hotmail.com" target='_blank' rel="noreferrer">Email me</a>
                 </article>
                 <article className='contact_option'>
                     <RiMessengerLine className='contact_option-icon'/>
                     <h4>Messenger</h4>
                     <h5>Nacho Tapia Troncoso</h5>
-                    <a href="https://m.me/nacho.t.troncoso" target='_blank'>Send a message</a>
+                    <a href="https://m.me/nacho.t.troncoso" target='_blank' rel="noreferrer">Send a message</a>
                 </article>
                 <article className='contact_option'>
                     <BsWhatsapp className='contact_option-icon'/>
                     <h4>WhatsApp</h4>
                     <h5>number</h5>
-                    <a href="https://api.whatsapp.com/send?phone=+46762428648" target='_blank'>Send a message</a>
+                    <a href="https://api.whatsapp.com/send?phone=+46762428648" target='_blank' rel="noreferrer">Send a message</a>
                 </article>
             </div>
             {/*END OF CONTACT OPTIONS*/}
-            <form action="">
+            <form ref={form} onSubmit={sendEmail}>
                 <input type="text" name='name' placeholder='Your Full Name' required />
                 <input type="email" name='email' placeholder='Your Email' required />
                 <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
--- my-portfolio/src/components/footer/Footer.jsx
+++ my-portfolio/src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {AiOutlineGithub} from 'react-icons/ai'
+import {FaFacebookSquare} from 'react-icons/fa'
+import {AiOutlineTwitter} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <a href="#" className='footer_logo'>AKA IGGE</a>
+
+        <ul className='permalinks'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#jobs">Work Experiences</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer_socials">
+            <a href="https://github.com/igge88"><AiOutlineGithub /></a>
+            <a href="https://www.facebook.com/nacho.t.troncoso"><FaFacebookSquare /></a>
+            <a href="https://twitter.com/"><AiOutlineTwitter /></a>
+        </div>
+
+        <div className='footer_copyrigth'>
+            <small>&copy; All rights reserved.</small>
+        </div>
+    </footer>
   )
 }
 
