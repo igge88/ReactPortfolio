4f26b934ca2fb69dbdcc49ca9424f956589dd120
igge <igge88@hotmail.com>
2023-04-23 21:12:55 +0200

    push

--- my-portfolio/src/components/about/AboutSection.js
+++ my-portfolio/src/components/about/AboutSection.js
@@ -9,7 +9,7 @@ import {Link} from 'react-router-dom'
 function About() {
   return (
     <section id='about'>
-        <h5>Get To Know</h5>
+
         <h2>About Me</h2>
 
         <div className='container about__container'>
