dcb47b7bc638358303c749e99f3db3a9f7598e3e
igge <igge88@hotmail.com>
2024-01-12 15:43:51 +0100

    commit

--- src/components/projects/ProjectsSection.js
+++ src/components/projects/ProjectsSection.js
@@ -79,106 +79,105 @@ const Portfolio = () => {
 export default Portfolio*/
 
 // VARIANT 2 With useEffect
-import React, { useEffect } from 'react';
-import './projectsStyles.css';
-import Bild1 from '../../assets/NewRetro.jpg';
-import Bild2 from '../../assets/R2D2.jpg';
-import Bild3 from '../../assets/Aventuras.jpg';
-import Bild4 from '../../assets/Figma.jpg';
-import Bild5 from '../../assets/SpotifyApp.jpg';
-import Bild6 from '../../assets/goodTalk.png';
+import React, { useEffect } from 'react'
+import './projectsStyles.css'
+import Bild1 from '../../assets/NewRetro.jpg'
+import Bild2 from '../../assets/R2D2.jpg'
+import Bild3 from '../../assets/Aventuras.jpg'
+import Bild4 from '../../assets/Figma.jpg'
+import Bild5 from '../../assets/Gomoku.png'
+import Bild6 from '../../assets/goodTalk.png'
 
 const data = [
-  {
-    id: 1,
-    image: Bild1,
-    title: 'A HTML & CSS website',
-    github: 'https://github.com/igge88/Lab1',
-    demo: 'https://new-retro.netlify.app/',
-  },
-  {
-    id: 2,
-    image: Bild2,
-    title: 'A VUE project with APIs',
-    github: 'https://github.com/igge88/R2D2App',
-    demo: 'https://igge88.github.io/R2D2App/',
-  },
-  {
-    id: 3,
-    image: Bild3,
-    title: 'A group project with VUE',
-    github: 'https://github.com/vikluund/Team1',
-    demo: 'https://github.com/vikluund/Team1',
-  },
-  {
-    id: 4,
-    image: Bild4,
-    title: 'A UX/UI group project - Designsystem',
-    github: 'https://github.com/igge88',
-    demo: 'https://figma.com/',
-  },
-  {
-    id: 5,
-    image: Bild5,
-    title: 'A JavaScript project - Spotify API',
-    github: 'https://github.com/igge88/Lab2',
-    demo: 'https://github.com/igge88/Lab2',
-  },
-  {
-    id: 6,
-    image: Bild6,
-    title: 'A Fullstack project - Chat Web application',
-    github: 'https://github.com/igge88/Lab2',
-    demo: 'https://github.com/igge88/Lab2',
-  },
-];
+    {
+        id: 1,
+        image: Bild1,
+        title: 'A HTML & CSS website',
+        github: 'https://github.com/igge88/Lab1',
+        demo: 'https://new-retro.netlify.app/'
+    },
+    {
+        id: 2,
+        image: Bild2,
+        title: 'A VUE project with APIs',
+        github: 'https://github.com/igge88/R2D2App',
+        demo: 'https://igge88.github.io/R2D2App/'
+    },
+    {
+        id: 3,
+        image: Bild3,
+        title: 'A group project with VUE',
+        github: 'https://github.com/igge88/Team1-Aventuras',
+        demo: 'https://aventuras-proj.netlify.app/'
+    },
+    {
+        id: 4,
+        image: Bild4,
+        title: 'A UX/UI group project - Designsystem',
+        github: 'https://github.com/igge88',
+        demo: 'https://figma.com/'
+    },
+    {
+        id: 5,
+        image: Bild5,
+        title: 'A group project with React - Gomoku',
+        github: 'https://github.com/igge88/Gomoku',
+        demo: 'https://gomoku-proj.netlify.app/'
+    },
+    {
+        id: 6,
+        image: Bild6,
+        title: 'A Fullstack project - Chat Web application',
+        github: 'https://github.com/igge88/Chat-App',
+        demo: 'https://goodtalk.netlify.app/'
+    }
+]
 
 const Portfolio = () => {
-  useEffect(() => {
-    console.log('Portfolio component mounted');
-  }, []);
+    useEffect(() => {
+        console.log('Portfolio component mounted')
+    }, [])
 
-  return (
-    <section id="portfolio">
-      <h5>My Recent Projects</h5>
-      <h2>Portfolio</h2>
+    return (
+        <section id="portfolio">
+            <h5>My Recent Projects</h5>
+            <h2>Portfolio</h2>
 
-      <div className="container portfolio_container">
-        {data.map(({ id, image, title, github, demo }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} />
-              </div>
-              <h3>{title}</h3>
-              <div className="portfolio_item-cta">
-                <a
-                  href={github}
-                  className="btn"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Github
-                </a>
-                <a
-                  href={demo}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Demo
-                </a>
-              </div>
-            </article>
-          );
-        })}
-      </div>
-    </section>
-  );
-};
+            <div className="container portfolio_container">
+                {data.map(({ id, image, title, github, demo }) => {
+                    return (
+                        <article key={id} className="portfolio__item">
+                            <div className="portfolio__item-image">
+                                <img src={image} alt={title} />
+                            </div>
+                            <h3>{title}</h3>
+                            <div className="portfolio_item-cta">
+                                <a
+                                    href={github}
+                                    className="btn"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    Github
+                                </a>
+                                <a
+                                    href={demo}
+                                    className="btn btn-primary"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    Demo
+                                </a>
+                            </div>
+                        </article>
+                    )
+                })}
+            </div>
+        </section>
+    )
+}
 
-export default Portfolio;
-
+export default Portfolio
 
 /* VARIANT 3
 import React, { useEffect, useState } from 'react';
